515708fd77c445c84938fb3f89f756c8cd319f7c
Ronald van der Kooij <ronald@sharpsolutions.nl>
2014-07-19 14:18:54 +0200

    mocking the require in the users controller to spec the retrieval of the index view

--- specs/server/admin/userscontrollerSpecs.js
+++ specs/server/admin/userscontrollerSpecs.js
@@ -1,24 +1,31 @@
-// var assert = require("assert");
+var assert = require("assert");
 
-// var ResponseObject = function(){
-// 	this.render = function(view, options){
-// 		this.view = view;
-// 		this.options = options;
-// 	};
-// }
+var ResponseObject = function(){
+	this.render = function(view, options){
+		this.view = view;
+		this.options = options;
+	};
+}
 
-// describe('User controller specs:', function(){
+
+describe('User controller specs:', function(){
 	
-// 	describe('When requesting the index,', function(){
+	var expectRequire = require('a').expectRequire;
+
+	describe('When requesting the index,', function(){
 		
-// 		var controller = require('../../../controllers/admin/userscontroller.js');
-// 		var responseObject = new ResponseObject();
+		var fakeRepository = function(){ };
+		
+		expectRequire('../../bin/repos/usersRepository').return(fakeRepository);
 
-// 		controller.index(null, responseObject);
+		var controller = require('../../../controllers/admin/userscontroller.js');
+		var responseObject = new ResponseObject();
 
-// 		it('It should render the index with the standard layout', function(){
-// 			assert.equal(responseObject.view, 'admin/users/index');
-// 			assert.equal(responseObject.options.layout, 'admin/shared/layout');
-// 		});
-// 	});
-// });
+		controller.index(null, responseObject);
+
+		it('It should render the index with the standard layout', function(){
+			assert.equal(responseObject.view, 'admin/users/index');
+			assert.equal(responseObject.options.layout, 'admin/shared/layout');
+		});
+	});
+});
