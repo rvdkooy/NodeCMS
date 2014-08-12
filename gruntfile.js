397c6ac8d87e52a474dd8828f446db5acded48d9
Ronald van der Kooij <ronald@sharpsolutions.nl>
2014-08-12 20:08:52 +0200

    added a watch task to the grunt file that will run all the server specs again when a spec file is changed in the specs/server folder

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,19 @@
 module.exports = function(grunt) {
 
+  grunt.initConfig({
+    watch: {
+      
+      // src: {
+      //   files: ['lib/*.js', 'css/**/*.scss', '!lib/dontwatch.js'],
+      //   tasks: ['default'],
+      // },
+      servertests: {
+        files: 'specs/server/**/*.js',
+        tasks: ['servertests'],
+      }
+    }
+  });
+
   grunt.registerTask('servertests', 'run server tests', function () {
     var done = this.async();
     require('child_process').exec('npm test', function (err, stdout) {
@@ -8,5 +22,8 @@ module.exports = function(grunt) {
     });
   });
 
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('test', ['servertests', 'watch']);
   grunt.registerTask('default', ['servertests']);
 };
