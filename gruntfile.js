6d6ef63e8d8572f0064b86c07203eee038b09d04
Ronald van der Kooij <ronald@sharpsolutions.nl>
2014-08-12 14:27:46 +0200

    added the mocha server test to the initial grunt file

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,14 @@
 module.exports = function(grunt) {
 
-  grunt.registerTask('default', '1st task', function() {
-    grunt.log.write('this is my first grunt task ').ok();
-  });
+  // grunt.registerTask('default', '1st task', function() {
+  //   grunt.log.write('this is my first grunt task ').ok();
+  // });
 
+  grunt.registerTask('default', 'run mocha tests', function () {
+    var done = this.async();
+    require('child_process').exec('npm test', function (err, stdout) {
+      grunt.log.write(stdout);
+      done(err);
+    });
+  });
 };
