b82e2e0cdce36f06cf35f0e4e83d3eb1279b7c6c
ronald van der kooij <ronald@sharpsolutions.nl>
2014-09-16 00:39:51 +0200

    fixed a bug where the logs app dir was ignored

---
+++ system/assets/admin/scripts/apps/logs/logsApp.js
@@ -0,0 +1,6 @@
+﻿angular.module('logsApp', ['services', 'ngResource', 'sharedmodule'])
+.controller('logsController', ['$scope', 'logsService',
+    function ($scope, logsService) {
+
+        $scope.logs = logsService.query({ limit: 75 });
+    }]);
